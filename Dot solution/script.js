7c5f7fe83c23bcf5241b4e1d76237a201a1188bd
Skyler Wiernik <skylerwiernik@gmail.com>
2020-07-29 14:37:58 -0400

    Initial commit

---
+++ Dot solution/script.js
@@ -0,0 +1,32 @@
+const dot = "<img src='dot.png' height='50'></img>" //Let kids make the dots bigger or smaller if they want
+
+async function start() {
+    var dots = ""
+    const number = random(25, 100) //Let kids choose this range
+    for (var i = 0; i < number; i++) {
+        dots += dot
+    }
+    document.getElementById("container").innerHTML = dots
+
+    await wait(3) //Let kids choose how long to wait
+
+    document.getElementById("container").innerHTML = ""
+
+    var guess = window.prompt("How many dots was that?")
+    if (guess == number) {
+        window.alert("Nice job! You got it!")
+    }
+    else {
+        window.alert("Close! It was " + number)
+    }
+}
+
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function wait(seconds) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
